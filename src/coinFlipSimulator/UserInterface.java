178f0c3c4ddc449a6ee2a02ad4d72cbccfc19637
Austin Sullivan <austinmclastro@gmail.com>
2017-04-23 15:50:50 -0600

    Animated main menu line working!

--- src/coinFlipSimulator/UserInterface.java
+++ src/coinFlipSimulator/UserInterface.java
@@ -7,32 +7,42 @@ import java.util.Scanner;
  * @author Austin
  */
 public class UserInterface {
+	
 	private UserInterface instance = null;
-	private String line1 = "------------------";
-	private String line2 = "--------  --------";
-	private String line3 = "-------    -------";
-	private String line4 = "------      ------";
-	private String line5 = "-----        -----";
-	private String line6 = "----          ----";
-	private String line7 = "---            ---";
-	private String line8 = "--              --";
-	private String line9 = "-                -";
+	
+	
+	private String line0 = "------------------";
+	private String line1 = "--------  --------";
+	private String line2 = "-------    -------";
+	private String line3 = "------      ------";
+	private String line4 = "-----        -----";
+	private String line5 = "----          ----";
+	private String line6 = "---            ---";
+	private String line7 = "--              --";
+	private String line8 = "-                -";
+	
+	private String[] menuLines = new String[] {line0, line1, line2, line3, line4, line5, line6, line7, line8};
+	
+	private boolean end = false;		//flag which is set when the program is to end
+	
 	
 	
 	/**
 	 * Constructor method for user interface of game.
 	 */
 	protected UserInterface(){
+		
 		if(instance == null){
-			System.out.println("======== Coin Simulator 2017 ========");
-			System.out.println("         by Austin Sullivan         \n");
+			System.out.println("\n======== Coin Flip Simulator 2017 ========");
+			System.out.println("           by Austin Sullivan         \n");
 			mainMenu();
 		}
 	}
 	
 	private void mainMenu(){
+		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Main Menu");
+		System.out.println("    Main Menu");
 		animation();
 		
 	}
@@ -42,27 +52,26 @@ public class UserInterface {
 	/**
 	 * Method which uses a new thread to create animated interfaces
 	 */
-	private void animation(){
+	private synchronized void animation(){
 		
 		Thread anim = new Thread(){
 			
 			public void run(){
-				int lineState = 1;
-				String theLine = "";
+				
+				int lineState = 0;
 				boolean incr = true;		//when true, lineState is incremented at end of while loop
 											//when false, lineState is decremented at end of while loop
 				
-				while(!this.isInterrupted()){
-					theLine = "line" + lineState;
-					System.out.println(theLine);
+				while(!end){
 					
-					//TODO: make regex or something to print the specifically defind lines (at top of class)
-					//		instead of printing 'line1', 'line2', etc.
-					//TODO: make lines replace themselves on one line, instead of printing to a new line every time.
+					System.out.print(menuLines[lineState]+"\r");
 					
+					
+					//logic to control fluctuating menu line (which index of the lines to print)
+					//----------------------------------------------------
 					if(incr){
 						lineState++;
-						if(lineState == 10)
+						if(lineState == 8)
 							incr = false;
 					}
 					else{
@@ -72,12 +81,17 @@ public class UserInterface {
 					}
 					
 					try {
-						this.sleep(400);
+						this.sleep(100);
 					} catch (InterruptedException e) {
 						System.out.println("animation thread interrupted!");
 						e.printStackTrace();
 					}
+					//----------------------------------------------------
+					
+					
 				}
+				
+				
 			}
 		};
 		anim.start();
